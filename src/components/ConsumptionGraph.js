d656f783e3cf37d2aad20c3565aa3e1726dfbc1d
Ingo Sterzinger <ingo@unibright.io>
2019-04-02 20:20:37 +0200

    port toolchain to react-create-app

--- src/components/ConsumptionGraph.js
+++ src/components/ConsumptionGraph.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Chart from 'react-apexcharts'
-import Moment from 'react-moment'
+// import Moment from 'react-moment'
 
 class ConsumptionGraph extends Component {
 
